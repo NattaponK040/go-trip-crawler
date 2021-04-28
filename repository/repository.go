3ef7e1cc1069d247c098ae6616290501801cc398
NattaponK040 <62997126+NattaponK040@users.noreply.github.com>
2021-04-28 19:33:06 +0700

    Update repository.go

--- repository/repository.go
+++ repository/repository.go
@@ -8,12 +8,6 @@ import (
 
 type MgRepository struct {
 	ctx            context.Context
-	wnTravel       *mongo.Collection
-	wnTravelReview *mongo.Collection
-	wnHotel        *mongo.Collection
-	wnHotelReview  *mongo.Collection
-	wnShop         *mongo.Collection
-	wnShopReview   *mongo.Collection
 
 	tripTravel       *mongo.Collection
 	tripTravelReview *mongo.Collection
@@ -28,13 +22,7 @@ type MgRepository struct {
 func NewMongoRepository(db *mongo.Client, ctx context.Context, mcf *config.Mongodb) *MgRepository {
 	return &MgRepository{
 		ctx:            ctx,
-		wnTravel:       db.Database(mcf.DBname).Collection(mcf.WonnaiTravelData),
-		wnTravelReview: db.Database(mcf.DBname).Collection(mcf.WonnaiTravelReview),
-		wnHotel:        db.Database(mcf.DBname).Collection(mcf.WonnaiHotelData),
-		wnHotelReview:  db.Database(mcf.DBname).Collection(mcf.WonnaiHotelReview),
-		wnShop:         db.Database(mcf.DBname).Collection(mcf.WonnaiShopData),
-		wnShopReview:   db.Database(mcf.DBname).Collection(mcf.WonnaiShopReview),
-
+		
 		tripTravel:       db.Database(mcf.DBname).Collection(mcf.TripadvisorTravelData),
 		tripTravelReview: db.Database(mcf.DBname).Collection(mcf.TripadvisorTravelReview),
 		tripHotel:        db.Database(mcf.DBname).Collection(mcf.TripadvisorHotelData),
